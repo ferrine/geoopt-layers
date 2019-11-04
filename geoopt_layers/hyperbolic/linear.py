d4aa5e59febe08f449a66227f27aff8c12cc15d7
Max Kochurov <maxim.v.kochurov@gmail.com>
2019-11-04 18:45:30 +0300

    add tests for linear

--- geoopt_layers/hyperbolic/linear.py
+++ geoopt_layers/hyperbolic/linear.py
@@ -3,19 +3,40 @@ import geoopt
 
 
 class MobiusLinear(torch.nn.Linear):
-    def __init__(self, *args, ball=None, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        bias=True,
+        *,
+        ball: geoopt.PoincareBall,
+        ball_out=None
+    ):
+        super().__init__(in_features=in_features, out_features=out_features, bias=bias)
         # for manifolds that have parameters like Poincare Ball
         # we have to attach them to the closure Module.
         # It is hard to implement device allocation for manifolds in other case.
         self.ball = ball
+        if ball_out is None:
+            if not in_features == out_features:
+                raise ValueError(
+                    "Please explicitly provide ball_out if it not the same"
+                )
+            ball_out = ball
+        self.ball_out = ball_out
         if self.bias is not None:
-            self.bias = geoopt.ManifoldParameter(self.bias, manifold=self.ball)
+            self.bias = geoopt.ManifoldParameter(self.bias, manifold=self.ball_out)
         self.reset_parameters()
 
     def forward(self, input):
         self.ball.assert_attached(input)
-        return mobius_linear(input, weight=self.weight, bias=self.bias, ball=self.ball)
+        return mobius_linear(
+            input,
+            weight=self.weight,
+            bias=self.bias,
+            ball=self.ball,
+            ball_out=self.ball_out,
+        )
 
     @torch.no_grad()
     def reset_parameters(self):
@@ -23,8 +44,22 @@ class MobiusLinear(torch.nn.Linear):
             self.bias.zero_()
 
 
-def mobius_linear(input, weight, bias=None, *, ball: geoopt.PoincareBall):
-    output = ball.mobius_matvec(weight, input)
-    if bias is not None:
-        output = ball.mobius_add(output, bias)
+def mobius_linear(
+    input,
+    weight,
+    bias=None,
+    *,
+    ball: geoopt.PoincareBall,
+    ball_out: geoopt.PoincareBall
+):
+    if ball is ball_out:
+        output = ball.mobius_matvec(weight, input)
+        if bias is not None:
+            output = ball.mobius_add(output, bias)
+    else:
+        tangent = ball.logmap0(input)
+        new_tangent = tangent @ weight
+        output = ball_out.expmap0(new_tangent)
+        if bias is not None:
+            output = ball_out.mobius_add(output, bias)
     return output
--- tests/test_hyperbolic.py
+++ tests/test_hyperbolic.py
@@ -1,1 +1,30 @@
-import pytest
+import geoopt
+import geoopt_layers
+
+
+def test_linear_same_ball():
+    ball = geoopt.PoincareBall()
+    point = ball.random(2, 3, 5)
+    layer = geoopt_layers.hyperbolic.MobiusLinear(5, 5, ball=ball)
+    out = layer(point)
+    ball.assert_attached(out)
+    ball.assert_check_point_on_manifold(out)
+
+
+def test_linear_new_ball():
+    ball = geoopt.PoincareBall()
+    ball2 = geoopt.PoincareBall()
+    point = ball.random(2, 3, 5)
+    layer = geoopt_layers.hyperbolic.MobiusLinear(5, 5, ball=ball, ball_out=ball2)
+    out = layer(point)
+    ball2.assert_attached(out)
+    ball2.assert_check_point_on_manifold(out)
+
+
+def test_linear_new_ball1():
+    ball = geoopt.PoincareBall()
+    point = ball.random(2, 3, 5)
+    layer = geoopt_layers.hyperbolic.MobiusLinear(5, 5, ball=ball, ball_out=ball)
+    out = layer(point)
+    ball.assert_attached(out)
+    ball.assert_check_point_on_manifold(out)
