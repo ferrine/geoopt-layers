3b3de893e5373414c52f9c7ebd2b85a017e32709
Maxim Kochurov <m.kochurov@partner.samsung.com>
2019-11-05 18:36:01 +0300

    test distance alignment

--- tests/test_distance_layers.py
+++ tests/test_distance_layers.py
@@ -21,3 +21,18 @@ def test_distance_pairwise():
     points = man.random(10, 3, 9)
     distances = layer(points)
     assert distances.shape == (10, 3, 3)
+
+
+def test_distance_pairwise_paired():
+    man = geoopt.Sphere()
+    layer = geoopt_layers.PairwiseDistances(dim=0)
+    points = man.random(10, 9)
+    points1 = man.random(7, 9)
+    distances = layer(points, points1)
+    assert distances.shape == (10, 7)
+
+    layer = geoopt_layers.PairwiseDistances(dim=-3)
+    points = man.random(10, 3, 9)
+    points1 = man.random(7, 3, 9)
+    distances = layer(points, points1)
+    assert distances.shape == (10, 7, 3)
