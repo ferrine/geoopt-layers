9a37517cb9b484e48152256cc6e59f6c70d11372
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2020-03-17 22:23:42 +0300

    init

--- geoopt_layers/poincare/hyperplanes.py
+++ geoopt_layers/poincare/hyperplanes.py
@@ -86,7 +86,7 @@ class Distance2PoincareHyperplanes(ManifoldModule):
     def reset_parameters(self):
         torch.nn.init.xavier_normal_(self.tangents)
         if self.scale is not None:
-            self.scale.fill_(1.0)
+            self.scale.fill_(1e-2)
         self.tangents.proj_()
         torch.nn.init.constant_(self.log_dist0, -5)
 
